0d65979c3d9a69c9f7f1ab8b2362eb3baa0f9143
SHIHCHANG <axray@hotmail.com>
2020-04-15 17:40:17 +0800

    change style

--- src/components/Masks/Mask/Mask.js
+++ src/components/Masks/Mask/Mask.js
@@ -12,7 +12,7 @@ const mask = (props) => (
                 <p><strong>小孩數量：</strong>{props.info.childCount}</p>
                 <p><strong>備註：</strong>{props.info.note}</p>
                 <p><strong>更新時間：</strong>{props.info.updated}</p>
-                <p><a href={`https://www.google.com/maps/search/?api=1&query=${props.info.latitude},${props.info.longitude}`}>位置</a></p>
+                <p><storng>位置連結：</storng><a href={`https://www.google.com/maps/search/?api=1&query=${props.info.latitude},${props.info.longitude}`}>位置</a></p>
             </div>
         </article>
     )
